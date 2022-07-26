f1213a1303a8bd24af3dd91f21f32f11331d5689
ClaraSantosmf <clarasantosmf@gmail.com>
2022-07-26 13:26:54 -0300

    class method. Created Class people and method compliment

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,10 @@
 #Classe é a base da orientação objeto, pois ela cria objetos. Utilizando CamelCase.
-
+# método é uma função da classe e está atrelada ao objeto. Sua escrit aé com underline. Sempre terá o parâmetro do self.
+# self é o próprio objeto que será criado passado implicitamente no método
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Olá'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
