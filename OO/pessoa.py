db916ebf36451f7391216193f076dc51f8357f54
ClaraSantosmf <clarasantosmf@gmail.com>
2022-07-26 14:06:11 -0300

    class about complex attributes

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -3,9 +3,11 @@
 # self é o próprio objeto que será criado passado implicitamente no método
 # Atributos são criados após o método especial de inicialização __initi(self): logo abaixo com self.atributo o parâmetro é depois do =, nesse caso, 'nome'
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self,  *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
+        self.filhos =list(filhos)
+
 
     def cumprimentar(self):
         return 'Olá'
@@ -17,6 +19,10 @@ if __name__ == '__main__':
 # Para alterar o atributo de um objeto criado, é possível retribuir chamando objeto . atributo e =
     p.nome = 'Clara'
     print(p.nome, p.idade)
-    p = Pessoa('Maria', 24)
+    p = Pessoa(nome='Maria', idade=24)
     print(p.nome)
     print(p.idade)
+    jessica = Pessoa(nome='Jessica')
+    marilia = Pessoa(jessica, nome='Marilia')
+    for filho in marilia.filhos:
+        print(filho.nome)
